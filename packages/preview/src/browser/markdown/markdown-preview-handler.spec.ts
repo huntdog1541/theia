2d367a6c8ce547b11701ea4080dffabc955ba49a
Alex Tugarev <alex.tugarev@typefox.io>
2018-01-22 17:42:53 +0100

    fix tests
    Signed-off-by: Alex Tugarev <alex.tugarev@typefox.io>

--- packages/preview/src/browser/markdown/markdown-preview-handler.spec.ts
+++ packages/preview/src/browser/markdown/markdown-preview-handler.spec.ts
@@ -7,14 +7,13 @@
 
 import * as chai from 'chai';
 import { expect } from 'chai';
-import { PreviewHandler } from '../preview-handler';
 import { MarkdownPreviewHandler } from './markdown-preview-handler';
 
 const jsdom = require('jsdom-global');
 
 chai.use(require('chai-string'));
 
-let previewHandler: PreviewHandler;
+let previewHandler: MarkdownPreviewHandler;
 
 before(() => {
     jsdom();
@@ -77,10 +76,10 @@ See [here](https://github.com/theia-ide/theia).
 `;
 
 const exampleHtml1 = //
-    `<h1 class="line" data-line="0">Theia - Preview Extension</h1>
+    `<h1 id="theia-preview-extension" class="line" data-line="0">Theia - Preview Extension</h1>
 <p class="line" data-line="1">Shows a preview of supported resources.
 See <a href="https://github.com/theia-ide/theia">here</a>.</p>
-<h2 class="line" data-line="4">License</h2>
+<h2 id="license" class="line" data-line="4">License</h2>
 <p class="line" data-line="5"><a href="https://github.com/theia-ide/theia/blob/master/LICENSE">Apache-2.0</a></p>
 `;
 
